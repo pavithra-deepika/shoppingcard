0644f7452d3870604cb9e6af0f180babec0c616b
pavithra <pavithradeepika301@gmail.com>
2023-10-16 15:33:44 +0530

    card display function completed.

---
+++ src/Component/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='flex shopping-cart'>
+        <div> Shopping Cart App</div>
+        <div>
+            Cart
+        <sup>{'3'}</sup>
+        </div>
+    </div>
+  )
+}
+
+export default Header
